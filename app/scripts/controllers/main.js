307712341309d48cc548a7a4f219170600f25b20
Francisco Gutierrez <fsalvador23@gmail.com>
2015-03-30 14:43:57 -0600

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name valoraWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the valoraWebappApp
+ */
+angular.module('valoraWebappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
