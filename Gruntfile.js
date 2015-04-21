e67b4126720d04535753e415c114316c33cd679d
Francisco Gutierrez <fsalvador23@gmail.com>
2015-04-21 03:04:08 -0500

    updated app

--- Gruntfile.js
+++ Gruntfile.js
@@ -343,8 +343,7 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*',
-            'bower_components/**'
+            'styles/fonts/{,*/}*.*'
           ]
         }, {
           expand: true,
