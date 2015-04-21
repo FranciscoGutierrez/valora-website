9a3938f030d3592ce092bd9b4ff0675f87d01acb
Francisco Gutierrez <fsalvador23@gmail.com>
2015-04-21 04:10:43 -0500

    Updated app

--- Gruntfile.js
+++ Gruntfile.js
@@ -337,7 +337,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,js,min.js}',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
