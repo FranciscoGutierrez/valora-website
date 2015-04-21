84e7418a5f586fc508e00ec6905e5d967e6e6d83
Francisco Gutierrez <fsalvador23@gmail.com>
2015-04-21 02:44:52 -0500

    Updated App

--- Gruntfile.js
+++ Gruntfile.js
@@ -375,9 +375,10 @@ module.exports = function (grunt) {
         'copy:styles'
       ],
       dist: [
-        'copy:styles',
-        'imagemin',
-        'svgmin'
+        'copy:styles'
+        // 'copy:styles',
+        // 'imagemin',
+        // 'svgmin'
       ]
     },
 
